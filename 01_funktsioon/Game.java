51e4f9a3bb8da38963e84ae432899bf9fabc7e47
Karl <karlthefish@github.com>
2022-02-11 15:13:27 +0200

    Mängija liikumine

--- 01_funktsioon/Game.java
+++ 01_funktsioon/Game.java
@@ -1,12 +1,48 @@
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
         System.out.println("Test");
 
         int worldHeight = 5;
         int worldWidth = 10;
+        int playerX = generateWorldCoords(worldWidth);
+        int playerY = generateWorldCoords(worldHeight);
+        int enemyX = generateWorldCoords(worldWidth);
+        int enemyY = generateWorldCoords(worldHeight);
+
+        printMap(worldHeight, worldWidth, playerX, playerY, enemyX, enemyY);
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("end")) {
+            switch (input) { //Switch kontrollib ühte muutujat mingite väärtuste vastu
+                case "a":
+                    playerX--;
+                    break;
+                case "s":
+                    playerY++;
+                    break;
+                case "d":
+                    playerX++;
+                    break;
+                case "w":
+                    playerY--;
+            }
+            printMap(worldHeight, worldWidth, playerX, playerY, enemyX, enemyY);
+            input = scanner.nextLine();
+        }
+    }
+
+    public static int generateWorldCoords(int worldSize){
+        return (int) (Math.random()*(worldSize - 2)) + 1;
+    }
+
+    public static void printMap(int worldHeight, int worldWidth, int playerX, int playerY, int enemyX, int enemyY){
         char symbol;
 
-        for (int y  = 0; y  < worldHeight; y ++) {
+        for (int y = 0; y  < worldHeight; y ++) {
             for (int x = 0; x < worldWidth; x++) {
                 if (y == 0 || y == worldHeight - 1) {
                     symbol = '-';
@@ -15,10 +51,15 @@ public class Game {
                 } else {
                     symbol = ' ';
                 }
+                if(playerY == y && playerX == x){
+                    symbol = 'x';
+                }
+                if(enemyY == y && enemyX == x){
+                    symbol = '0';
+                }
                 System.out.print(symbol);
             }
             System.out.println();
         }
     }
-
 }
