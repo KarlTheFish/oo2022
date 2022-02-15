421c04037a5ac214566d1b3d4e84f61bf794e632
Karl <karlthefish@github.com>
2022-02-15 17:02:28 +0200

    Kodutöö algus

--- 01_funktsioon/Functions.java
+++ 01_funktsioon/Functions.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Functions {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String x = input.nextLine();
+        String y = input.nextLine();
+        int answer = Add(x,y);
+        System.out.println(answer);
         
     }
 
+    public static int Add(int x, int y){
+        int answer = x + y;
+        return answer;
+    }
 }
