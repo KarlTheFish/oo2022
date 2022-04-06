2aa14e4c9364993be6edc39d208166818eb52d30
Karl <karlthefish@github.com>
2022-04-06 23:18:45 +0300

    tunnitöö

---
+++ KarlPood/src/test/java/ee/karl/karlpood/KarlPoodApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.karl.karlpood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarlPoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
